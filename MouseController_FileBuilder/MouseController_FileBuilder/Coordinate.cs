8534ca9701f36d8e81d2eeeb0d2c2f6a15a80f9d
jgates <jgates1995@hotmail.com>
2017-02-25 16:42:35 -0500

    Initial commit

---
+++ MouseController_FileBuilder/MouseController_FileBuilder/Coordinate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseController_FileBuilder
+{
+    public class Coordinate
+    {
+        public int OrderId { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static List<Coordinate> GetCoordinates(string fileName)
+        {
+            List<Coordinate> coordinates = new List<Coordinate>();
+
+            int count = 0;
+            string line = string.Empty;
+            char[] comma = { ',' };
+            string[] valueList = null;
+            int OrderId,
+                X,
+                Y;
+            int indexOrderId = -1,
+                indexX = -1,
+                indexY = -1;
+
+            using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var sr = new StreamReader(fileStream))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        line = sr.ReadLine();
+                        valueList = line.Split(comma);
+                        if (count++ == 0)
+                        {
+                            int index = 0;
+                            foreach (string v in valueList)
+                            {
+                                string value = "";
+                                if (!string.IsNullOrEmpty(v)) value = v.Trim().ToLowerInvariant();
+                                switch (value)
+                                {
+                                    case "order":
+                                        indexOrderId = index;
+                                        break;
+                                    case "x":
+                                        indexX = index;
+                                        break;
+                                    case "y":
+                                        indexY = index;
+                                        break;
+                                }
+                                index++;
+
+                                if (indexOrderId >= 0 && indexX >= 0 && indexY >= 0)
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            if (!(indexOrderId >= 0 && indexX >= 0 && indexY >= 0))
+                            {
+                                throw new Exception("Not all expected columns are present!");
+                            }
+
+                            if (!int.TryParse(valueList[indexOrderId], out OrderId))
+                                throw new Exception("Order is not a valid integer!");
+                            if (!int.TryParse(valueList[indexX], out X))
+                                throw new Exception("X is not a valid integer!");
+                            if (!int.TryParse(valueList[indexY], out Y))
+                                throw new Exception("Y is not a valid integer!");
+
+                            coordinates.Add(new Coordinate() { OrderId = OrderId, X = X, Y = Y });
+                        }
+
+                    }
+                }
+            }
+
+            return coordinates;
+        }
+
+    }
+
+}
---
+++ MouseController_FileBuilder/MouseController_FileBuilder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MouseController_FileBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputFile = string.Empty;
+            string outputFile = string.Empty;
+
+            foreach(string arg in args)
+            {
+                if(arg.Split(new char[] { '=' })[0] == "/input")
+                {
+                    inputFile = arg.Split(new char[] { '=' })[1];
+                }
+                else if (arg.Split(new char[] { '=' })[0] == "/output")
+                {
+                    inputFile = arg.Split(new char[] { '=' })[1];
+                }
+            }
+
+            List<Coordinate> coordinates = Coordinate.GetCoordinates(inputFile);
+
+            XmlWriterSettings settings = new XmlWriterSettings() { Indent = true };
+            using (XmlWriter writer = XmlWriter.Create(outputFile, settings))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("ArrayOfMouseEvents", "http://schemas.datacontract.org/2004/07/MouseController");
+                writer.WriteAttributeString("xmlns", "i", null, "http://www.w3.org/2001/XMLSchema-instance");
+
+                coordinates.ForEach(c => WriteMouseClick(writer, c));
+                
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+            }
+            //Console.ReadLine();
+        }
+
+        static void WriteMouseClick(XmlWriter writer, int order, int x, int y)
+        {
+            WriteMouseEvent(writer, "WM_LBUTTONDOWN", order.ToString(), x.ToString(), y.ToString(), "450000");
+            WriteMouseEvent(writer, "WM_LBUTTONUP", string.Empty, x.ToString(), y.ToString(), "110000");
+        }
+
+        static void WriteMouseClick(XmlWriter writer, Coordinate coordinate)
+        {
+            WriteMouseClick(writer, coordinate.OrderId, coordinate.X, coordinate.Y);
+        }
+
+        static void WriteMouseEvent(XmlWriter writer, string message, string tag, string x, string y, string offset)
+        {
+            writer.WriteStartElement("MouseEvents");
+            writer.WriteElementString("MouseMsg", message);
+            writer.WriteElementString("Tag", tag);
+
+
+            writer.WriteStartElement("pos");
+            writer.WriteAttributeString("xmlns", "a", null, "http://schemas.datacontract.org/2004/07/System.Drawing");
+            writer.WriteElementString("a", "x", null, x);
+            writer.WriteElementString("a", "y", null, y);
+            writer.WriteEndElement();
+            writer.WriteElementString("tOffset", offset.ToString());
+            writer.WriteEndElement();
+        }
+    }
+}
